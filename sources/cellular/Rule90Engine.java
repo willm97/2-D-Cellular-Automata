8620beea03e22068afc20b2206762b7c5b90fb59
willm97 <willm97@vt.edu>
2019-02-24 10:05:39 -0500

    Initial commit that adds all the resources files and the source code, including the bash shell commands. Maybe soon I will get this program to look more formal somewhere, I want to put it online evnetually. I'll hopefully put in some more work in a moment.

---
+++ sources/cellular/Rule90Engine.java
@@ -0,0 +1,40 @@
+package cellular;
+
+public class Rule90Engine extends Engine
+{
+	public Rule90Engine(int states)
+	{
+		super(states);
+	}
+	/**
+	 *  Computes the next state of the Cell from the rule for this type.
+	 */
+	@Override
+    public void computeNextState(Cell[] neighbors)
+    {
+        // Here's where the fun stuff happens!
+    	// neighbors is an array of the adjacent cells, like this:
+    	// 8 5 2
+    	// 7 4 1
+    	// 6 3 0
+    	// Where 4 is the current cell.
+        // I interpolated edges a bit.
+        if (neighbors[0] == null && neighbors[6] == null)
+        {
+            neighbors[4].setNextState(neighbors[4].getState());
+        }
+        else
+        {
+            if (neighbors[0] == null)
+            {
+                neighbors[0] = new Cell(0);
+            }
+            if (neighbors[6] == null)
+            {
+                neighbors[6] = new Cell(0);
+            }
+            neighbors[4].setNextState(
+                neighbors[0].getState() ^ neighbors[6].getState());
+        }
+    }
+}
