9c4cc227137e85b021bf8f27b47903b6c3833eec
William McDermott <willm97@vt.edu>
2019-06-15 10:27:17 -0400

    File keeping, miniscule ideas on cellular V2 if I get to it.

---
+++ sources/cellular2/GridLoader.java
@@ -0,0 +1,14 @@
+package cellular2;
+
+import cellular.*;
+// Default package?
+
+/**
+ *  This class should load preset grids for each Automata given a name.
+ *  It would ideally be able to do this using the save and load features
+ *  of Program.
+ */
+public class GridLoader
+{
+	// TODO
+}
---
+++ sources/cellular2/Main.java
@@ -0,0 +1,22 @@
+package cellular2;
+
+import cellular.*;
+
+/**
+ *  Runner class for the updated library. Should be like 2 dense lines.
+ */
+public class Main
+{
+    /**
+     *  Entry point of version 2 of the application.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        // Call GUI
+        // Call Simulation / Engine
+        // whatever else
+    }
+}
+
+
